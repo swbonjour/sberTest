04dc7980c5c606c1896dae770e3c5279d206635e
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-11-01 23:46:40 +0300

    cors

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const dbConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'mysql_server',
+  host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'root',
+  password: '901256KKkk',
   database: 'config',
   entities: [__dirname + './../../dist/entities/*.entity.js'],
   synchronize: true,
